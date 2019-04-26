a525c32637f791981648ec5aaa43c034a2b9b205
tom_global <646450308@qq.com>
2019-04-26 18:52:26 +0800

    Site updated: 2019-04-26 18:52:19

--- js/main.js
+++ js/main.js
@@ -143,3 +143,66 @@ var Main = (function(){
 })();
 
 $(Main.init());
+
+
+
+
+
+var evt={
+	//  想获取 clientX/Y  通过 e或者 window.event 获取
+	//  window 和 e 的封装,  因为火狐不支持window.event   ie8 不支持事件参数e
+	getEvent:function(e){
+		return window.event||e;
+	},
+
+	//  获取可视区域的横纵坐标的兼容代码
+	//  根据浏览器支持的 是 事件参数e 还是 window.event ,动态的选择
+	getClientX:function(e){
+		return this.getEvent(e).clientX;
+	},
+
+	//可视区域的纵坐标的兼容代码
+	getClientY:function (evt) {
+		return this.getEvent(evt).clientY;
+	},
+
+	// 获取当前页面向左滚动出去的距离
+	getScrollLeft:function(){
+		// 不同的浏览器 获取 scroll属性的值是不同的   , 没有的话就是没滚动, 0
+		return window.pageXOffset||document.body.scrollLeft||document.documentElement.scrollLeft||0;
+	},
+
+	//页面向上卷曲出去的纵坐标
+	getScrollTop:function () {
+		return window.pageYOffset||document.body.scrollTop||document.documentElement.scrollTop||0;
+	},
+
+	// 获取到浏览器的滚动条滚动后, 鼠标相对于滚动后的浏览器的顶部,左部的值
+	//  pageX  ==  clientX +　scrollLeft
+	getPageX:function(e){
+		// 问 pageX 能用不? 能用直接返回 , 不能用  返回  clientX + scrollLeft
+		// pageX IE不支持
+		return this.getEvent(e).pageX ?  this.getEvent(e).pageX : this.getClientX(e) + this.getScrollLeft(e);
+	},
+
+	//相对于页面的纵坐标(pageY或者是clientY+scrollTop)
+	getPageY:function (evt) {
+		return this.getEvent(evt).pageY?this.getEvent(evt).pageY:this.getClientY(evt)+this.getScrollTop();
+	}
+};
+
+
+// 终极图片飞
+// 为浏览器注册鼠标移动事件
+document.onmousemove=function(e){
+	// 获取img ,改变他的  top  left
+	// 使用的公式是  left = clientX + scrollLeft
+  //  console.log(1);
+document.getElementsByClassName("show").style.left=evt.getPageX(e)+"px";
+document.getElementsByClassName("show").style.top=evt.getPageY(e)+"px";
+ 
+};
+
+
+
+
